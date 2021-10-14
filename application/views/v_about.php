a02d54e6d17d9e99cd977d01abd60bb560b20ff1
wahyuningtyas <tyastriutami45@gmail.com>
2021-10-14 16:22:52 +0800

    upload project 3A

---
+++ application/views/v_about.php
@@ -0,0 +1,27 @@
+<section>
+    <h1><?php echo $judul ?></h1>
+    <h4>Nama</h4>
+    <ul type="disc">
+        <li>Nama Depan : WAHYUNINGTYAS</li>
+        <li>Nama Belakang : TRI UTAMI</li>
+    </ul>
+    <br>
+    <h4>Alamat</h4>
+    <ul type="none">
+        <li> Purwokerto</li>
+    </ul>
+    
+    <h4>Tempat Lahir</h4>
+    <ul type="none">
+        <li>Banjarnegara</li>
+    </ul>
+    <h4>Hobby</h4>
+    <ul type="square">
+        <li>Tidur siang</li>
+    </ul>
+    <br>
+    <h4>Hewan Favorite</h4>
+    <ul type="square">
+        <li>Buaya</li>
+    </ul>
+</section>
